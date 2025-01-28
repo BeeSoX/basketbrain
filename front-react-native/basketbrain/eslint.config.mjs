5de6fb43c995fd8925323a1e97407ce0919039be
RomainR <romain.rethore@gmail.com>
2025-01-28 14:20:44 +0100

    BA-4 : CI : pipeline linter         Ajout d'un fichier de config pour eslint

---
+++ front-react-native/basketbrain/eslint.config.mjs
@@ -0,0 +1,34 @@
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+), {
+    plugins: {
+        react,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+
+    rules: {},
+}];
