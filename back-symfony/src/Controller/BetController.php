a15154f674657375531a99c150c0eacae2d23f0e
RomainR <romain.rethore@gmail.com>
2025-01-29 15:53:09 +0100

    BA 5 : betController         Création de routes pour l'API

--- back-symfony/src/Controller/BetController.php
+++ back-symfony/src/Controller/BetController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BetRepository;
 use App\Entity\Bet;
+use App\Repository\UserRepository;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Entity;
@@ -24,12 +25,34 @@ class BetController extends AbstractController
         return new JsonResponse($allbets, 200, [], true);
     }
 
+    #[Route('/bets/{id}', name: 'app_bet_user', methods: ['GET'])]
+    public function show(BetRepository $betRepository, UserRepository $userRepository, SerializerInterface $serializer, $id): JsonResponse
+    {
+        $user = $userRepository->find($id);
+        $bet = $betRepository->findBy(['user' => $user]);
+        $bet = $serializer->serialize($bet, 'json');
+        $data = json_decode($bet, true); // Decode JSON into an associative array
+
+        // Remove the "user" entry from each object
+        foreach ($data as &$bet) {
+            unset($bet['user']);
+        }
+
+        // Re-encode the modified data back to JSON
+        $bet = json_encode($data, JSON_PRETTY_PRINT);
+        return new JsonResponse($bet, 200, [], true);
+    }
+
     #[Route('/bet', name: 'app_bet_create', methods: ['POST'])]
     public function create(EntityManagerInterface $entityManager, SerializerInterface $serializer, Request $request): JsonResponse
     {
+        $postData = json_decode($request->getContent(), true);
+
         $data = $serializer->deserialize($request->getContent(), Bet::class, 'json');
+        $user_id = $postData['user']['id'];
+        $user = $entityManager->getRepository(User::class)->find($user_id);
         $bet = new Bet();
-        $bet->setUser($this->getUser());
+        $bet->setUser($user);
         $bet->setBetDate($data->getBetDate());
         $bet->setBetGame($data->getBetGame());
         $bet->setBetWin($data->getBetWin());
